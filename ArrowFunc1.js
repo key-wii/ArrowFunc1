a8dbe09c9f5c27d55b2d091a3ae4ae15da528a3b
Christopher Gomez <programmerchristopher@gmail.com>
2018-08-10 21:51:00 -0700

    Arrow Function Part 1

--- ArrowFunc1.js
+++ ArrowFunc1.js
@@ -1,4 +1,4 @@
-var input = process.argv.slice(2);
-var result = inputs.map()
-    .reduce();
-console.log();
+var inputs = process.argv.slice(2);
+var result = inputs.map( x => x[0] )
+                    .reduce( (memo, x) => memo + x, '' );
+console.log(`[${inputs}] becomes "${result}"`);
