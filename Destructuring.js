940688f9c7b5d860b2b050824ee03551aa70d0ce
Christopher Gomez <programmerchristopher@gmail.com>
2018-08-14 18:38:30 -0700

    Destructuring

--- Destructuring.js
+++ Destructuring.js
@@ -1,3 +1,6 @@
 let userArray = process.argv.slice(2);
-
-console.log(`{username: "${}", email: "${}"}`);
+let username = userArray.map( list => list[1] )
+    .reduce( (x, list) => x + list);
+let email = userArray.map( list => list[2] )
+    .reduce( (x, list) => x + list);
+console.log(`{username: "${username}", email: "${email}"}`);
